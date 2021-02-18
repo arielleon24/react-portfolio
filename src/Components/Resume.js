99a2620a22949f9a4ba96bc9455846c4b2ad4a92
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-02-18 00:06:32 -0500

    fixed orders in skills

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -27,7 +27,7 @@ class Resume extends Component {
 
       <div className="row education">
          <div className="three columns header-col">
-            <h1><span>Education:</span></h1>
+            <h1><span>Education</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -43,7 +43,7 @@ class Resume extends Component {
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>Experiences:</span></h1>
+            <h1><span>Experiences</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -56,7 +56,7 @@ class Resume extends Component {
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>Skills:</span></h1>
+            <h1><span>Skills</span></h1>
          </div>
 
          <div className="nine columns main-col">
